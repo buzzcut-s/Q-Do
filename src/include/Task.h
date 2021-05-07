d8b92940fe29938e9cf325df175ed9e1e9cfee6e
Piyush Kumar <piyushkumg@gmail.com>
2021-05-07 19:55:33 +0530

    Update Task class

--- src/include/Task.h
+++ src/include/Task.h
@@ -1,6 +1,7 @@
 #ifndef TASK_H
 #define TASK_H
 
+#include <QString>
 #include <QWidget>
 
 namespace Ui
@@ -13,8 +14,12 @@ class Task : public QWidget
         Q_OBJECT
 
 public:
-        explicit Task(QWidget* parent = nullptr);
+        explicit Task(const QString& name, QWidget* parent = nullptr);
         ~Task();
+
+        void    setName(const QString& name);
+        QString name() const;
+        bool    isCompleted() const;
 
 private:
         Ui::Task* ui;
